724b8c416bbc3c133c4a21bcf35cb24ef25c8715
xushenchen <767199918@qq.com>
2021-02-02 22:35:18 +0800

    更新了vant的库，修改了个人中心的颜色

--- litemall-wx/lib/vant-weapp/mixins/basic.js
+++ litemall-wx/lib/vant-weapp/mixins/basic.js
@@ -1,22 +1,11 @@
 export const basic = Behavior({
-    methods: {
-        $emit(...args) {
-            this.triggerEvent(...args);
-        },
-        getRect(selector, all) {
-            return new Promise(resolve => {
-                wx.createSelectorQuery()
-                    .in(this)[all ? 'selectAll' : 'select'](selector)
-                    .boundingClientRect(rect => {
-                    if (all && Array.isArray(rect) && rect.length) {
-                        resolve(rect);
-                    }
-                    if (!all && rect) {
-                        resolve(rect);
-                    }
-                })
-                    .exec();
-            });
-        }
-    }
+  methods: {
+    $emit(name, detail, options) {
+      this.triggerEvent(name, detail, options);
+    },
+    set(data) {
+      this.setData(data);
+      return new Promise((resolve) => wx.nextTick(resolve));
+    },
+  },
 });
